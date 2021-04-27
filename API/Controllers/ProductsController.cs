cb7665a7145ecfd3378a32ae6f7e8134bdd5f64a
Pushpa Raj Dangi <dcpushparaj@gmail.com>
2021-04-27 16:24:12 +0545

    initialCreate

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<ActionResult<List<Product>>> GetProductsAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
